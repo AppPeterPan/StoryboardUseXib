f46b2828ad21eaea72e0dc43b3c732633b799985
Shr Ying Pan <apppeterpan@gmail.com>
2018-03-03 16:49:14 +0800

    Initial Commit

---
+++ StoryboardUseXib/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StoryboardUseXib
+//
+//  Created by SHIH-YING PAN on 03/03/2018.
+//  Copyright © 2018 SHIH-YING PAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
